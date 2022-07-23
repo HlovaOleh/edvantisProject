3354c185dbc64aa0199a76410b1fe0f27694c2bc
Hlova Oleh <hlovaoleh0@gmail.com>
2022-07-23 13:18:17 +0300

    Fix conflict

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-<<<<<<< Updated upstream
-import logo from './logo.svg';
-import './App.css';
-=======
 import Header from "./components/Header";
 import AppContainer from "./components/AppContainer";
 import MainInfoContainer from './components/MainInfoContainer'
->>>>>>> Stashed changes
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Header />
+      <MainInfoContainer />
+    </AppContainer>
   );
 }
 
